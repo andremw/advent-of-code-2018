bd27835fdefe4d5bebd9457a930d6fe5f6a0ce59
André <mazoni.andre@gmail.com>
2018-12-07 12:13:29 -0200

    adds initial structure for day-2

---
+++ day-2/tests/Tests.elm
@@ -0,0 +1,12 @@
+module Tests exposing (tests)
+
+import Expect
+import Test exposing (..)
+
+describe "Day 2 - Inventory Management System"
+    [ describe "Calculates the checksum for lists of box IDs"
+        [ test "name" <|
+            \_ ->
+
+        ]
+    ]
