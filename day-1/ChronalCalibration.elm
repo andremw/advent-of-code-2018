41dca827af671cedde36417d9350757d3471f96f
André <mazoni.andre@gmail.com>
2018-12-07 11:40:39 -0200

    fixes function making all tests pass

--- day-1/ChronalCalibration.elm
+++ day-1/ChronalCalibration.elm
@@ -1,12 +1,75 @@
 module ChronalCalibration exposing
-  ( calculateResultingFrequency
-  , findFrequencyThatRepeatsTwice
-  )
+    ( calculateResultingFrequency
+    , findFirstFrequencyThatRepeatsTwice
+    )
+
+import Set exposing (..)
+
 
 calculateResultingFrequency : List Int -> Int
 calculateResultingFrequency frequencies =
-  List.sum frequencies
+    List.sum frequencies
 
-findFrequencyThatRepeatsTwice : List Int -> Int
-findFrequencyThatRepeatsTwice frequencies =
-  0
+
+findFirstFrequencyThatRepeatsTwice : List Int -> Int
+findFirstFrequencyThatRepeatsTwice frequencies =
+    firstFrequencyHelper frequencies frequencies 0 (Set.insert 0 Set.empty)
+
+
+firstFrequencyHelper : List Int -> List Int -> Int -> Set Int -> Int
+firstFrequencyHelper originalFrequencies partialFrequencies currentFrequency achievedFrequencies =
+    let
+        nextFrequency =
+            Maybe.withDefault 0 (List.head partialFrequencies)
+
+        newFrequency =
+            currentFrequency + nextFrequency
+
+        wasNewFrequencyAlreadyAchieved =
+            Set.member newFrequency achievedFrequencies
+
+        -- _ =
+        --     Debug.log "Achieved Frequencies" achievedFrequencies
+        -- _ =
+        --     Debug.log "Is repeated" wasNewFrequencyAlreadyAchieved
+        -- _ =
+        --     Debug.log ""
+        --         ("Current frequency "
+        --             ++ String.fromInt currentFrequency
+        --             ++ ", change of "
+        --             ++ String.fromInt nextFrequency
+        --             ++ "; resulting frequency "
+        --             ++ String.fromInt newFrequency
+        --         )
+    in
+    if wasNewFrequencyAlreadyAchieved then
+        newFrequency
+
+    else
+        let
+            newAchievedFrequencies =
+                Set.insert newFrequency achievedFrequencies
+
+            nextPartialFrequencies =
+                List.drop 1 partialFrequencies
+
+            hasReachedEndOfList =
+                List.isEmpty nextPartialFrequencies
+
+            -- _ =
+            --     Debug.log "Partial frequencies" partialFrequencies
+        in
+        -- if we have reached the end of list we start again
+        if hasReachedEndOfList then
+            firstFrequencyHelper
+                originalFrequencies
+                originalFrequencies
+                newFrequency
+                newAchievedFrequencies
+
+        else
+            firstFrequencyHelper
+                originalFrequencies
+                nextPartialFrequencies
+                newFrequency
+                newAchievedFrequencies
--- day-1/tests/Tests.elm
+++ day-1/tests/Tests.elm
@@ -1,42 +1,71 @@
 module Tests exposing (tests)
 
+import ChronalCalibration exposing (..)
 import Expect
 import Test exposing (..)
 
-import ChronalCalibration exposing (..)
 
 tests : Test
 tests =
-  describe "Day 1 - Chronal Calibration"
-    [ describe "Calculates the resulting frequency given a list of frequencies"
-        [ test "Given a simple list" <|
-            \_ ->
-                let
-                  list = [1, 1, 1]
-                in
-                  Expect.equal 3 (calculateResultingFrequency list)
-        ,
-          test "Given another simple list" <|
-              \_ ->
-                  let
-                    list = [1, 1, -2]
-                  in
+    describe "Day 1 - Chronal Calibration"
+        [ describe "Calculates the resulting frequency given a list of frequencies"
+            [ test "Given a simple list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 1, 1, 1 ]
+                    in
+                    Expect.equal 3 (calculateResultingFrequency list)
+            , test "Given another simple list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 1, 1, -2 ]
+                    in
                     Expect.equal 0 (calculateResultingFrequency list)
-        ,
-          test "Given yet another simple list" <|
-              \_ ->
-                  let
-                    list = [-1, -2, -3]
-                  in
+            , test "Given yet another simple list" <|
+                \_ ->
+                    let
+                        list =
+                            [ -1, -2, -3 ]
+                    in
                     Expect.equal -6 (calculateResultingFrequency list)
+            ]
+        , describe "Finds the first frequency that repeats twice"
+            [ test "Example list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 1, -2, 3, 1 ]
+                    in
+                    Expect.equal 2 (findFirstFrequencyThatRepeatsTwice list)
+            , test "First list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 1, -1 ]
+                    in
+                    Expect.equal 0 (findFirstFrequencyThatRepeatsTwice list)
+            , test "Second list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 3, 3, 4, -2, -4 ]
+                    in
+                    Expect.equal 10 (findFirstFrequencyThatRepeatsTwice list)
+            , test "Third list" <|
+                \_ ->
+                    let
+                        list =
+                            [ -6, 3, 8, 5, -6 ]
+                    in
+                    Expect.equal 5 (findFirstFrequencyThatRepeatsTwice list)
+            , test "Fourth list" <|
+                \_ ->
+                    let
+                        list =
+                            [ 7, 7, -2, -7, -4 ]
+                    in
+                    Expect.equal 14 (findFirstFrequencyThatRepeatsTwice list)
+            ]
         ]
-    ,
-      describe "Finds the first frequency that repeats twice"
-          [ test "Given a simple list" <|
-              \_ ->
-                let
-                  list = [1, -1]
-                in
-                  Expect.equal 0 (findFrequencyThatRepeatsTwice list)
-          ]
-    ]
