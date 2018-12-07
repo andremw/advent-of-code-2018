ad1cbc16f4772f8c8d995d7969f197dda7a6d755
André <mazoni.andre@gmail.com>
2018-12-07 17:04:38 -0200

    starting to create a structure

--- day-2/InventoryManagement.elm
+++ day-2/InventoryManagement.elm
@@ -1,1 +1,17 @@
-module InventoryManagement exposing (..)
+module InventoryManagement exposing (calculateChecksum)
+
+import Tuple exposing (..)
+
+
+calculateChecksum : List String -> Int
+calculateChecksum ids =
+    let
+        result =
+            calculateAppearances ""
+    in
+    (*) (Tuple.first result) (Tuple.second result)
+
+
+calculateAppearances : String -> ( Int, Int )
+calculateAppearances id =
+    ( 4, 3 )
--- day-2/tests/Tests.elm
+++ day-2/tests/Tests.elm
@@ -1,12 +1,21 @@
 module Tests exposing (tests)
 
+import DayInput exposing (dayInput)
 import Expect
+import InventoryManagement exposing (..)
 import Test exposing (..)
 
-describe "Day 2 - Inventory Management System"
-    [ describe "Calculates the checksum for lists of box IDs"
-        [ test "name" <|
-            \_ ->
 
+tests : Test
+tests =
+    describe "Day 2 - Inventory Management System"
+        [ describe "Calculates the checksum for lists of box IDs"
+            [ test "Example list" <|
+                \_ ->
+                    let
+                        list =
+                            [ "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" ]
+                    in
+                    Expect.equal 12 (calculateChecksum list)
+            ]
         ]
-    ]
