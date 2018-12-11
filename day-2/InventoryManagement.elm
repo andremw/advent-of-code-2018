135a0a84747ebf692fed63c0c461eabc13e6a12d
André <mazoni.andre@gmail.com>
2018-12-10 22:07:20 -0200

    Makes test for checksum calculation pass

--- day-2/InventoryManagement.elm
+++ day-2/InventoryManagement.elm
@@ -1,17 +1,22 @@
-module InventoryManagement exposing (calculateChecksum)
+module InventoryManagement exposing (hasNDuplicateLetters)
 
-import Tuple exposing (..)
-
-
-calculateChecksum : List String -> Int
-calculateChecksum ids =
-    let
-        result =
-            calculateAppearances ""
-    in
-    (*) (Tuple.first result) (Tuple.second result)
+import Dict exposing (..)
 
 
-calculateAppearances : String -> ( Int, Int )
-calculateAppearances id =
-    ( 4, 3 )
+hasNDuplicateLetters : Int -> String -> Bool
+hasNDuplicateLetters n word =
+    word
+        |> String.toList
+        |> List.foldl keepOccurrences Dict.empty
+        |> Dict.filter (\key value -> value == n)
+        |> Dict.isEmpty
+        |> not
+
+
+keepOccurrences : comparable -> Dict comparable Int -> Dict comparable Int
+keepOccurrences item occurrences =
+    if Dict.member item occurrences then
+        Dict.update item (Maybe.map ((+) 1)) occurrences
+
+    else
+        Dict.insert item 1 occurrences
--- day-2/tests/Tests.elm
+++ day-2/tests/Tests.elm
@@ -1,21 +1,42 @@
 module Tests exposing (tests)
 
 import DayInput exposing (dayInput)
+import Dict exposing (..)
 import Expect
-import InventoryManagement exposing (..)
+import InventoryManagement exposing (hasNDuplicateLetters)
 import Test exposing (..)
 
 
 tests : Test
 tests =
     describe "Day 2 - Inventory Management System"
-        [ describe "Calculates the checksum for lists of box IDs"
-            [ test "Example list" <|
+        [ describe "hasNDuplicateLetters"
+            [ test "Checks that \"abcdef\" doesn't have 2 duplicates" <|
+                \_ ->
+                    Expect.equal False (hasNDuplicateLetters 2 "abcdef")
+            , test "Checks that \"abbcde\" has two duplicates" <|
+                \_ ->
+                    Expect.equal True (hasNDuplicateLetters 2 "abbcde")
+            , test "Checks that \"abbcde\" doesn't have three duplicates" <|
+                \_ ->
+                    Expect.equal False (hasNDuplicateLetters 3 "abbcde")
+            , test "Checks that \"bababc\" has three duplicates" <|
+                \_ ->
+                    Expect.equal True (hasNDuplicateLetters 3 "bababc")
+            ]
+        , describe "Calculate Checksum"
+            [ test "From example input" <|
                 \_ ->
                     let
-                        list =
+                        input =
                             [ "abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab" ]
+
+                        with2Duplicates =
+                            List.filter (hasNDuplicateLetters 2) input
+
+                        with3Duplicates =
+                            List.filter (hasNDuplicateLetters 3) input
                     in
-                    Expect.equal 12 (calculateChecksum list)
+                    Expect.equal 12 (List.length with2Duplicates * List.length with3Duplicates)
             ]
         ]
