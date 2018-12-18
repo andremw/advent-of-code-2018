f359ff7bf460e44abdb6fed83568060df83566cc
André <mazoni.andre@gmail.com>
2018-12-18 16:52:37 -0200

    finishes part 2 of day 5

--- day-5/AlchemicalReduction.elm
+++ day-5/AlchemicalReduction.elm
@@ -1,68 +1,137 @@
-module AlchemicalReduction exposing (getRemainingUnits)
+module AlchemicalReduction exposing (getRemainingUnits, getShortestPolymer)
+
+import Set exposing (Set)
 
 
 getRemainingUnits : String -> String
 getRemainingUnits originalUnits =
     originalUnits
         |> String.toList
-        |> List.map Char.toCode
-        |> helper []
-        |> List.map Char.fromCode
-        |> String.fromList
+        |> List.foldl reduce []
+        |> List.map String.fromChar
+        |> List.reverse
+        |> String.join ""
 
 
-helper : List Int -> List Int -> List Int
-helper remaining tokens =
-    -- let
-    --     _ =
-    --         Debug.log "helper call" (formatToDebug tokens remaining)
-    -- in
-    case tokens of
-        first :: second :: tail ->
-            let
-                difference =
-                    first - second
+getShortestPolymer : String -> String
+getShortestPolymer originalUnits =
+    let
+        -- reduce the amount of work by calling the solution from the previous exercise
+        remainingUnits : String
+        remainingUnits =
+            getRemainingUnits originalUnits
 
-                _ =
-                    if difference == 32 || difference == -32 then
-                        Debug.log "got difference" ("size is " ++ String.fromInt (List.length tokens))
+        uniqueLowerChars : Set Char
+        uniqueLowerChars =
+            remainingUnits
+                |> String.toList
+                |> List.filter Char.isLower
+                |> Set.fromList
 
-                    else
-                        ""
+        -- _ =
+        --     Debug.log "unique lower chars" uniqueLowerChars
+        unitsWithoutEachChar : List String
+        unitsWithoutEachChar =
+            uniqueLowerChars
+                |> Set.toList
+                |> List.map (\char -> getUnitsWithoutChar char (String.toList remainingUnits))
 
-                -- _ =
-                --     Debug.log "tokens" tokens
-                -- _ =
-                --     Debug.log "remaining" remaining
-                -- _ =
-                --     Debug.log "comparing" (String.fromInt first ++ " - " ++ String.fromInt second ++ " = " ++ String.fromInt difference)
-            in
-            if difference == 32 || difference == -32 then
-                helper [] (remaining ++ tail)
+        -- _ =
+        --     Debug.log "units without each char" unitsWithoutEachChar
+        polymers : List String
+        polymers =
+            unitsWithoutEachChar
+                |> List.map getRemainingUnits
+                |> List.sortWith sortByLengthDesc
+
+        -- _ =
+        --     Debug.log "polymers" polymers
+    in
+    Maybe.withDefault "" (List.head polymers)
+
+
+sortByLengthDesc : String -> String -> Order
+sortByLengthDesc a b =
+    compare (String.length a) (String.length b)
+
+
+getUnitsWithoutChar : Char -> List Char -> String
+getUnitsWithoutChar char units =
+    units |> List.filter (\s -> Char.toLower s /= char) |> String.fromList
+
+
+reduce : Char -> List Char -> List Char
+reduce nextChar remaining =
+    case List.head remaining of
+        Nothing ->
+            [ nextChar ]
+
+        Just leftChar ->
+            if react leftChar nextChar then
+                List.drop 1 remaining
 
             else
-                helper (remaining ++ [ first ]) (second :: tail)
-
-        last :: [] ->
-            -- let
-            --     _ =
-            --         Debug.log "last" last
-            -- in
-            helper (remaining ++ [ last ]) []
-
-        [] ->
-            remaining
+                [ nextChar ] ++ remaining
 
 
-formatToDebug tokens remaining =
-    "helper" ++ formatList tokens ++ formatList remaining
+react : Char -> Char -> Bool
+react left right =
+    (Char.toLower left == Char.toLower right) && left /= right
 
 
-formatList : List Int -> String
-formatList list =
-    " [" ++ mapToChars list ++ "] "
 
-
-mapToChars : List Int -> String
-mapToChars list =
-    List.map Char.fromCode list |> String.fromList
+{-
+   The previous solution was very very very ineficient because it started at the beginning
+   of the string each time it found two chars that reacted
+-}
+-- getRemainingUnits : String -> String
+-- getRemainingUnits originalUnits =
+--     originalUnits
+--         |> String.toList
+--         |> List.map Char.toCode
+--         |> helper []
+--         |> List.map Char.fromCode
+--         |> String.fromList
+-- helper : List Int -> List Int -> List Int
+-- helper remaining tokens =
+--     -- let
+--     --     _ =
+--     --         Debug.log "helper call" (formatToDebug tokens remaining)
+--     -- in
+--     case tokens of
+--         first :: second :: tail ->
+--             let
+--                 difference =
+--                     first - second
+--                 _ =
+--                     if difference == 32 || difference == -32 then
+--                         Debug.log "got difference" ("size is " ++ String.fromInt (List.length tokens))
+--                     else
+--                         ""
+--                 -- _ =
+--                 --     Debug.log "tokens" tokens
+--                 -- _ =
+--                 --     Debug.log "remaining" remaining
+--                 -- _ =
+--                 --     Debug.log "comparing" (String.fromInt first ++ " - " ++ String.fromInt second ++ " = " ++ String.fromInt difference)
+--             in
+--             if difference == 32 || difference == -32 then
+--                 helper [] (remaining ++ tail)
+--             else
+--                 helper (remaining ++ [ first ]) (second :: tail)
+--         last :: [] ->
+--             -- let
+--             --     _ =
+--             --         Debug.log "last" last
+--             -- in
+--             helper (remaining ++ [ last ]) []
+--         [] ->
+--             remaining
+-- formatToDebug tokens remaining =
+--     "helper" ++ formatList tokens ++ formatList remaining
+-- formatList : List Int -> String
+-- formatList list =
+--     " [" ++ mapToChars list ++ "] "
+-- mapToChars : List Int -> String
+-- mapToChars list =
+--     List.map Char.fromCode list |> String.fromList
--- day-5/tests/Tests.elm
+++ day-5/tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (tests)
 
-import AlchemicalReduction exposing (getRemainingUnits)
+import AlchemicalReduction exposing (getRemainingUnits, getShortestPolymer)
 import DayInput exposing (dayInput)
 import Expect
 import Test exposing (..)
@@ -9,39 +9,50 @@ import Test exposing (..)
 tests : Test
 tests =
     describe "Day 5: Alchemical Reduction"
-        [ describe "Calculates remaining units after reaction"
-            [ skip <|
-                test "From 'aA'" <|
-                    \_ ->
-                        getRemainingUnits "aA" |> Expect.equal ""
-            , skip <|
-                test "From 'abBA'" <|
-                    \_ ->
-                        getRemainingUnits "abBA" |> Expect.equal ""
-            , skip <|
-                test "From 'abAB'" <|
-                    \_ ->
-                        getRemainingUnits "abAB" |> Expect.equal "abAB"
-            , skip <|
-                test "From 'aabAAB'" <|
-                    \_ ->
-                        getRemainingUnits "aabAAB" |> Expect.equal "aabAAB"
-            , skip <|
-                test "From example input" <|
-                    \_ ->
-                        let
-                            input =
-                                "dabAcCaCBAcCcaDA"
-                        in
-                        getRemainingUnits input
-                            |> String.length
-                            |> Expect.equal 10
+        [ describe "Part 1 - Calculates remaining units after reaction"
+            [ test "From 'aA'" <|
+                \_ ->
+                    getRemainingUnits "aA" |> Expect.equal ""
+            , test "From 'abBA'" <|
+                \_ ->
+                    getRemainingUnits "abBA" |> Expect.equal ""
+            , test "From 'abAB'" <|
+                \_ ->
+                    getRemainingUnits "abAB" |> Expect.equal "abAB"
+            , test "From 'aabAAB'" <|
+                \_ ->
+                    getRemainingUnits "aabAAB" |> Expect.equal "aabAAB"
+            , test "From example input" <|
+                \_ ->
+                    let
+                        input =
+                            "dabAcCaCBAcCcaDA"
+                    in
+                    getRemainingUnits input
+                        |> String.length
+                        |> Expect.equal 10
             , test "From exercise input" <|
                 \_ ->
                     let
                         input =
                             dayInput |> String.trim
                     in
-                    getRemainingUnits input |> String.length |> Expect.equal 10
+                    getRemainingUnits input |> String.length |> Expect.equal 11364
+            ]
+        , describe "Part two - Get shortest polymer"
+            [ test "From example input" <|
+                \_ ->
+                    let
+                        input =
+                            "dabAcCaCBAcCcaDA"
+                    in
+                    getShortestPolymer input |> String.length |> Expect.equal 4
+            , test "From exercise input" <|
+                \_ ->
+                    let
+                        input =
+                            dayInput
+                    in
+                    getShortestPolymer input |> String.length |> Expect.equal 4
             ]
         ]
